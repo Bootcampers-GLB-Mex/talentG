fc94af1ffa8c53e7cb3757669fb045a41eb54748
Giscam2501 <gis_joliee@hotmail.com>
2021-06-23 18:03:19 -0500

    agregue un css para main container

--- src/MainContainer/MainContainer.js
+++ src/MainContainer/MainContainer.js
@@ -4,16 +4,24 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import DailyClass from './DailyClass/DailyClass';
 import HomeworkFeedBack from './HomeworkFeedback/HomeworkFeedback';
+import Button from "./button1/button";
+import "./MainContainer.css";
+
 export default function MainContainer() {
    
 const [isTrainer] = useState(true);
     
 return (
-        <div>
+      <>  
             <Header/>
                 <DailyClass isTrainer={isTrainer} />
                 <HomeworkFeedBack/>
+            <div className= "ContainerButtons">
+                <Button children="Editar Perfil"></Button>
+                <Button children="Ver Agenda"></Button>
+                <Button children="Ver Feedback"></Button>
+            </div>
             <Footer/>
-        </div>
+        </>    
     )
 }

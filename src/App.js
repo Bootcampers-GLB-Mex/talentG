2d4e6ebadb542a86af0256ac086a58137d03a826
Giscam2501 <gis_joliee@hotmail.com>
2021-06-23 14:14:14 -0500

    agregue boton

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import DailyClass from './DailyClass/DailyClass';
 import HomeworkFeedback from './HomeworkFeedback/HomeworkFeedback';
+import {Button} from "./button1/button";
 
 function App() {
 
@@ -15,6 +16,19 @@ function App() {
       <Header />
       <DailyClass isTrainer={isTrainer} />
       <HomeworkFeedback/>
+      <div>
+        <div className= "App">
+            <Button>Editat Perfil</Button>
+        </div>
+        <div></div>
+        <div className= "App">
+            <Button>Ver Agenda</Button>
+        </div>
+        <div></div>
+        <div className= "App">
+                <Button>Ver Feedback</Button>
+        </div>
+      </div>
       <Footer />
     </div>
   );

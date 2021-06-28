595e7055c03d746549525b125fe44ea756570b81
Victor Cruz <vicc23@gmail.com>
2021-06-28 16:15:48 -0500

    Added links to navbar

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './header-styles.css';
 
 export default function Header() {
@@ -6,8 +7,8 @@ export default function Header() {
         <header>
             <p className="header__title">Talent</p>
             <img className="logoG" src={process.env.PUBLIC_URL + '/assets/img/logoG.png'} alt="logo"/>
-            <p className="my__bootcamp">Mi bootcamp</p>
-            <p className="my__profile">Mi perfil</p>
+            <Link to="/" className="my__bootcamp">Mi bootcamp</Link>
+            <Link to="/myProfile" className="my__profile">Mi perfil</Link>
         </header>
     )
 }
